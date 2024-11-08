e99a0afec151cc7bbaea1f54f42f039508aaf53f
bhumii-ii <99078842+bhumii-ii@users.noreply.github.com>
2024-11-08 22:20:40 +0530

    card added

--- merapp/src/App.js
+++ merapp/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Home from './screens/Home';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './screens/Login';
 
 function App() {
   return (
-   <>
-   <div> <Home/> </div>
-   </>
+   <Router>
+   <div>
+    <Routes>
+      <Route exact path='/' element={<Home/>}></Route>
+      <Route exact path='/login' element={<Login/>}></Route>
+    </Routes>
+   </div>
+   </Router>
   );
 }
 
